93df97b3c33ced6f386f829ae8dda0ab8f33d84f
Héctor Mauricio de la Rosa Prado <hoftherose@gmail.com>
2024-06-23 16:24:01 -0600

    Install lazygit plugin

--- nvim/lua/plugins/packer.lua
+++ nvim/lua/plugins/packer.lua
@@ -3,6 +3,12 @@ vim.cmd [[packadd packer.nvim]]
 return require('packer').startup(function(use)
 	use 'wbthomason/packer.nvim'
 	use ({ 'kevinhwang91/rnvimr', {run = 'make sync'} })
+	use ({
+		'kdheepak/lazygit.nvim',
+		requires = {
+			"nvim-lua/plenary.nvim",
+		},
+	})
 
 	use {
 		'nvim-telescope/telescope.nvim', tag = '0.1.4',
--- nvim/plugin/packer_compiled.lua
+++ nvim/plugin/packer_compiled.lua
@@ -74,6 +74,11 @@ end
 time([[try_loadstring definition]], false)
 time([[Defining packer_plugins]], true)
 _G.packer_plugins = {
+  ["lazygit.nvim"] = {
+    loaded = true,
+    path = "/home/HolyDemon/.local/share/nvim/site/pack/packer/start/lazygit.nvim",
+    url = "https://github.com/kdheepak/lazygit.nvim"
+  },
   ["packer.nvim"] = {
     loaded = true,
     path = "/home/HolyDemon/.local/share/nvim/site/pack/packer/start/packer.nvim",
