c1339573bd82f24726018870d95e006c1afc2066
Héctor Mauricio de la Rosa Prado <hoftherose@gmail.com>
2024-06-26 20:46:18 -0600

    Change harpoon shortcuts to e*

--- nvim/after/plugin/harpoon.lua
+++ nvim/after/plugin/harpoon.lua
@@ -1,11 +1,11 @@
 local mark = require('harpoon.mark')
 local ui = require('harpoon.ui')
 
-vim.keymap.set('n', '<leader>a', mark.add_file)
-vim.keymap.set('n', '<C-e>', ui.toggle_quick_menu)
+vim.keymap.set('n', '<leader>ea', mark.add_file)
+vim.keymap.set('n', '<leader>ee', ui.toggle_quick_menu)
 
-vim.keymap.set('n', '<C-1>', function() ui.nav_file(1) end)
-vim.keymap.set('n', '<C-2>', function() ui.nav_file(2) end)
-vim.keymap.set('n', '<C-3>', function() ui.nav_file(3) end)
-vim.keymap.set('n', '<C-4>', function() ui.nav_file(4) end)
+vim.keymap.set('n', '<leader>e1', function() ui.nav_file(1) end)
+vim.keymap.set('n', '<leader>e2', function() ui.nav_file(2) end)
+vim.keymap.set('n', '<leader>e3', function() ui.nav_file(3) end)
+vim.keymap.set('n', '<leader>e4', function() ui.nav_file(4) end)
 
