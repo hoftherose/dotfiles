5e50235a2b0b4a6f7386177157c26f35374ed6dc
Héctor Mauricio de la Rosa Prado <hoftherose@gmail.com>
2024-07-14 23:27:43 -0600

    Show dummy image GTK

--- dot_config/hypr/scripts/button.lua
+++ dot_config/hypr/scripts/button.lua
@@ -17,23 +17,26 @@ local lgi = require("lgi")
 local Gtk = lgi.require("Gtk", "4.0")
 local layer_shell = lgi.require("Gtk4LayerShell")
 local app = Gtk.Application {
-   application_id = "com.github.a-cloud-ninja.gtk4-layer-shell.lua-example",
+    application_id = "com.github.a-cloud-ninja.gtk4-layer-shell.lua-example",
 }
-app.on_activate = function()
-   local win = Gtk.Window {
-      application = app
-   }
-   layer_shell.init_for_window(win)
-   layer_shell.set_layer(win, layer_shell.Layer.BOTTOM)
-   layer_shell.set_anchor(win, layer_shell.Edge.BOTTOM, true)
-   layer_shell.set_exclusive_zone(win, 0)
-   local button = Gtk.Button {
-      label = "Gtk4 Layer Shell Example",
-   }
-   button.on_clicked = function()
-      win:close()
-   end
-   win:set_child(button)
-   win:present()
+
+function app:on_startup()
+    Gtk.ApplicationWindow({
+        application = self,
+        default_width = 400,
+        default_height = 400,
+    })
+end
+
+function app:on_activate()
+    layer_shell.init_for_window(self.active_window)
+    layer_shell.set_layer(self.active_window, layer_shell.Layer.BOTTOM)
+    layer_shell.set_anchor(self.active_window, layer_shell.Edge.BOTTOM, true)
+    layer_shell.set_exclusive_zone(self.active_window, 0)
+    local img = Gtk.Image({
+        visible = true, file = "icon.png"
+    })
+    self.active_window:set_child(img)
+    self.active_window:present()
 end
 app:run()
