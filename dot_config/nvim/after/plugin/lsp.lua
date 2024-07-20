d4323912d6999ba038e3cc4ec8b2bb3c062f9a8c
Héctor Mauricio de la Rosa Prado <hoftherose@gmail.com>
2024-07-19 21:58:21 -0600

    Move mason and add mason/formatter shortcuts

--- dot_config/nvim/after/plugin/lsp.lua
+++ dot_config/nvim/after/plugin/lsp.lua
@@ -6,14 +6,26 @@ lsp_zero.on_attach(function(client, bufnr)
   lsp_zero.default_keymaps({buffer = bufnr})
 end)
 
+
 -- to learn how to use mason.nvim
 -- read this: https://github.com/VonHeikemen/lsp-zero.nvim/blob/v3.x/doc/md/guide/integrate-with-mason-nvim.md
-require('mason').setup({})
-require('mason-lspconfig').setup({
-  ensure_installed = {},
+mason = require('mason')
+mason_lsp = require('mason-lspconfig')
+mason.setup({})
+mason_lsp.setup({
+  ensure_installed = {
+      "gopls",
+  },
   handlers = {
     function(server_name)
       require('lspconfig')[server_name].setup({})
     end,
   },
 })
+
+vim.keymap.set('n', '<leader>mo', '<cmd>Mason<CR>', {})
+-- https://www.reddit.com/r/neovim/comments/14gyqau/what_is_the_trick_theprimegen_is_using_to_search/
+vim.keymap.set("n", "<leader>mf", function()
+    vim.lsp.buf.format()
+end)
+
