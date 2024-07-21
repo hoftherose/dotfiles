94736966af7cc3dccc0848a2ebd9d02d9572f043
Héctor Mauricio de la Rosa Prado <hoftherose@gmail.com>
2024-07-21 02:24:26 -0600

    Mime filetype checker

---
+++ dot_config/nvim/after/plugin/mime.lua
@@ -0,0 +1,8 @@
+local function set_filetype(pattern, filetype)
+    vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+        pattern = pattern,
+        command = "set filetype=" .. filetype,
+    })
+end
+
+set_filetype({ "docker-compose.yml", "docker-compose.yaml" }, "yaml.docker-compose")
