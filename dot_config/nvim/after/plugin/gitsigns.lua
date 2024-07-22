8ddc5ca48cb4e8c7c41723dcff54cd6859b08b45
Héctor Mauricio de la Rosa Prado <hoftherose@gmail.com>
2024-07-21 18:20:04 -0600

    Gitsigns shortcuts

---
+++ dot_config/nvim/after/plugin/gitsigns.lua
@@ -0,0 +1,5 @@
+vim.keymap.set("n", "<leader>gp", "<cmd>Gitsigns preview_hunk<CR>", {})
+vim.keymap.set("n", "<leader>gP", "<cmd>Gitsigns preview_hunk_inline<CR>", {})
+vim.keymap.set("n", "<leader>gt", "<cmd>Gitsigns toggle_current_lint_blame<CR>", {})
+vim.keymap.set("n", "<leader>gj", "<cmd>Gitsigns next_hunk<CR>", {})
+vim.keymap.set("n", "<leader>gk", "<cmd>Gitsigns prev_hunk<CR>", {})
