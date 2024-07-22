eacd585bd68cf2625c86175f1925d87b796ab867
Héctor Mauricio de la Rosa Prado <hoftherose@gmail.com>
2024-07-21 20:49:11 -0600

    fix?

--- dot_config/nvim/after/plugin/lsp.lua
+++ dot_config/nvim/after/plugin/lsp.lua
@@ -81,15 +81,6 @@ require('mason-null-ls').setup({
     automatic_installation = true,
 })
 
-vim.diagnostic.config({
-    signs = {
-        enable = false,
-    },
-})
-vim.lsp.handlers['textDocument/publishDiagnostics'] = vim.lsp.with({
-    vim.lsp.diagnostic.on_publish_diagnostics, {
-        signs = false,
-    },
-})
+vim.g.lsp_zero_ui_signcolumn = 0
 vim.keymap.set('n', '<leader>mo', '<cmd>Mason<CR>')
 
