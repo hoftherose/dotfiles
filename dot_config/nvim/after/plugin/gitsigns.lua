79b8deb5106c9667cfa116ac2d90d42922818e5d
Héctor Mauricio de la Rosa Prado <hoftherose@gmail.com>
2024-07-24 20:16:06 -0600

    Fix gitblametoggle

--- dot_config/nvim/after/plugin/gitsigns.lua
+++ dot_config/nvim/after/plugin/gitsigns.lua
@@ -4,6 +4,6 @@ require('gitsigns').setup({
 
 vim.keymap.set("n", "<leader>gp", "<cmd>Gitsigns preview_hunk<CR>", {})
 vim.keymap.set("n", "<leader>gP", "<cmd>Gitsigns preview_hunk_inline<CR>", {})
-vim.keymap.set("n", "<leader>gt", "<cmd>Gitsigns toggle_current_lint_blame<CR>", {})
+vim.keymap.set("n", "<leader>gt", "<cmd>GitBlameToggle<CR>", {})
 vim.keymap.set("n", "<leader>gj", "<cmd>Gitsigns next_hunk<CR>", {})
 vim.keymap.set("n", "<leader>gk", "<cmd>Gitsigns prev_hunk<CR>", {})
