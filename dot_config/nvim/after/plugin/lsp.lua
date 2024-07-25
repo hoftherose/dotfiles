22a031d24561fc3ee4569bb0f7c116a044e68cc6
Héctor Mauricio de la Rosa Prado <hoftherose@gmail.com>
2024-07-25 00:18:59 -0600

    Fix keymaps

--- dot_config/nvim/after/plugin/lsp.lua
+++ dot_config/nvim/after/plugin/lsp.lua
@@ -82,9 +82,11 @@ require('mason-null-ls').setup({
     automatic_installation = true,
 })
 require("dapui").setup()
-
+vim.api.nvim_create_user_command("DapUiToggle", function()
+  require("dapui").toggle()
+end, {})
 vim.keymap.set('n', '<leader>mo', '<cmd>Mason<CR>')
 vim.keymap.set('n', '<leader>dt', '<cmd>DapUiToggle<CR>', {noremap=true})
-vim.keymap.set('n', '<leader>db', '<cmd>DapToggleBreakPoint<CR>', {noremap=true})
+vim.keymap.set('n', '<leader>db', '<cmd>DapToggleBreakpoint<CR>', {noremap=true})
 vim.keymap.set('n', '<leader>dc', '<cmd>DapContinue<CR>', {noremap=true})
 
