4f16e778877a5b73e103974804de4f79f2905e5c
Héctor de la Rosa Prado <hoftherose@gmail.com>
2026-09-18 18:34:45 -0600

    Fix serve only running in background

--- dot_config/nvim/lua/plugins.lua
+++ dot_config/nvim/lua/plugins.lua
@@ -290,7 +290,7 @@ require('lazy').setup({
                 vim.o.autoread = true
                 vim.keymap.set({ "n", "x" }, "<leader>ca", function() require("opencode").ask("@this: ", { submit = true }) end, { desc = "Ask opencode…" })
                 vim.keymap.set({ "n", "x" }, "<leader>cs", function() require("opencode").select() end,                          { desc = "Execute opencode action…" })
-                vim.keymap.set("n", "<leader>tc", function() require('snacks.terminal').toggle("opencode serve", {win={position='right',enter=false}}) end, { desc = "Toggle opencode" })
+                vim.keymap.set("n", "<leader>tc", function() require('snacks.terminal').toggle("opencode", {win={position='right',enter=false}}) end, { desc = "Toggle opencode" })
                 vim.keymap.set({ "n", "x" }, "<leader>cg",  function() return require("opencode").operator("@this ") end,        { desc = "Add range to opencode", expr = true })
                 vim.keymap.set("n",          "<leader>cG", function() return require("opencode").operator("@this ") .. "_" end, { desc = "Add line to opencode", expr = true })
             end,
